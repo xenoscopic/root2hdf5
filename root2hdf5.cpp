eb767ff6fa46a0c3476994a218ca9eb04ebcc0f6
Jacob Howard <jacob@havoc.io>
2013-07-31 16:59:35 +0200

    Began handling file manipulation logic.

--- root2hdf5.cpp
+++ root2hdf5.cpp
@@ -1,14 +1,15 @@
 // Standard includes
 #include <cstdlib>
 #include <iostream>
-#include <vector>
 #include <string>
 
 // Boost includes
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 // ROOT includes
-#include <TChain.h>
+#include <TROOT.h>
+#include <TFile.h>
 
 // HDF5 includes
 #include "H5Cpp.h"
@@ -19,6 +20,7 @@ using namespace std;
 
 // Boost namespace aliases
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 // HDF5 namespaces
 #ifndef H5_NO_NAMESPACE
@@ -30,30 +32,25 @@ using namespace H5;
 po::variables_map parse_command_line_options(int argc, char * argv[])
 {
     // Set up named arguments which are visible to the user
-    po::options_description visible(
-        "usage: root2hdf5 [options] <input-url> [<input-url>...] <output-url>"
+    po::options_description options(
+        "usage: root2hdf5 [options] <input-url> <output-url>"
     );
-    visible.add_options()
+    options.add_options()
+        ("input-url,i",
+            po::value<string>()->value_name("<input-url>")->required(),
+            "Input URL")
+        ("output-url,o",
+            po::value<string>()->value_name("<output-url>")->required(),
+            "Output URL")
+        ("overwrite,O", "Overwrite the output path.")
         ("verbose,v", "Print output of file operations.")
         ("help,h", "Print this message and exit.")
     ;
 
-    // Set up hidden arguments.  Anything we want to be positional needs to be
-    // specified in here.
-    po::options_description hidden("hidden options");
-    hidden.add_options()
-        ("urls,u",
-            po::value< vector<string> >()->multitoken(),
-            "URLs for input/output")
-    ;
-
-    // Create combined options
-    po::options_description all("all options");
-    all.add(visible).add(hidden);
-
     // Set up positional arguments
     po::positional_options_description p;
-    p.add("urls", -1);
+    p.add("input-url", 1);
+    p.add("output-url", 1);
 
     // Do the actual parsing
     po::variables_map vm;
@@ -61,7 +58,7 @@ po::variables_map parse_command_line_options(int argc, char * argv[])
     {
         po::store(
             po::command_line_parser(argc, argv)
-                .options(all)
+                .options(options)
                 .positional(p)
                 .run(),
             vm
@@ -71,28 +68,27 @@ po::variables_map parse_command_line_options(int argc, char * argv[])
         // Print help if requested
         if(vm.count("help"))
         {
-            cout << visible << endl;
+            cout << options << endl;
             exit(EXIT_SUCCESS);
         }
 
         // Print help if no (or not enough) paths were specified
-        if(vm.count("urls") == 0
-           || vm["urls"].as< vector<string> >().size() < 2)
+        if(vm.count("input-url") == 0 || vm.count("output-url") == 0)
         {
-            cout << visible << endl;
+            cout << options << endl;
             exit(EXIT_FAILURE);
         }
     }
     catch(std::exception& e)
     {
         cerr << "Couldn't parse command line options: " << e.what() << endl;
-        cerr << visible << endl;
+        cerr << options << endl;
         exit(EXIT_FAILURE);
     }
     catch(...)
     {
         cerr << "Couldn't parse command line options, not sure why." << endl;
-        cerr << visible << endl;
+        cerr << options << endl;
         exit(EXIT_FAILURE);
     }
     
@@ -102,15 +98,68 @@ po::variables_map parse_command_line_options(int argc, char * argv[])
 
 int main(int argc, char *argv[])
 {
-    // Parse command line options
+    // Set ROOT to batch mode so nothing pops up on the screen (not that it
+    // should, but you never know with ROOT)
+    gROOT->SetBatch(kTRUE);
+
+    // Parse command line options and create some convenient accessors
     po::variables_map options = parse_command_line_options(argc, argv);
+    bool verbose = options.count("verbose") > 0;
 
-    // Open the first file specified and use that as a map to seek out any TTree
-    // objects
+    // Grab file paths (parse_command_line_options will have validated them)
+    string input_url = options["input-url"].as<string>();
+    string output_url = options["output-url"].as<string>();
+
+    // Check if the output path exists.  If it does, and it is a directory, then
+    // the user has likely made a mistake, so bail.  If it does and it is a
+    // file, then check if the user has specified the overwrite option, and in
+    // that case, proceed.
+    fs::path output_path(output_url);
+    bool exists = fs::exists(output_path);
+    bool is_dir = exists && fs::is_directory(output_path);
+    if(exists)
+    {
+        if(is_dir)
+        {
+            cerr << "Output path is a directory, manually delete if you would "
+                 << "like to overwrite it" << endl;
+            exit(EXIT_FAILURE);
+        }
+        else if(options.count("overwrite") == 0)
+        {
+            cout << "Output path exists.  Specify the \"--overwrite\" option "
+                 << "if you would like to overwrite it" << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
     
+    // Print path information if requested
+    if(verbose)
+    {
+        cout << "Converting " << input_url << " -> " << output_url << endl;
+    }
+
+    // Open the input file
+    TFile *input_file = TFile::Open(input_url.c_str(), "READ");
+    if(input_file == NULL)
+    {
+        cerr << "Unable to open input file: " << input_url << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Open the output file
+    H5File *output_file = new H5File(output_url, H5F_ACC_TRUNC);
 
 
 
+    // Cleanup output resources
+    delete output_file;
+    output_file = NULL;
+
+    // Cleanup input resources
+    input_file->Close();
+    delete input_file;
+    input_file = NULL;    
 
     return 0;
 }
